9204365f14272be6c32fbc1fa71a114f94025450
Elfor <lshono8@gmail.com>
2021-05-09 06:53:07 +0300

    📱 Created firebase init error screen

---
+++ lib/screens/FirebaseInitErrorScreen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class FirebaseInitErrorScreen extends StatefulWidget {
+  final Object? error;
+
+  const FirebaseInitErrorScreen({Key? key, this.error}) : super(key: key);
+
+  @override
+  _FirebaseInitErrorScreenState createState() => _FirebaseInitErrorScreenState();
+}
+
+class _FirebaseInitErrorScreenState extends State<FirebaseInitErrorScreen> {
+  bool _showInfo = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Something went wrong'),
+            Text('Couldn\'t initialize Firebase app'),
+            SizedBox(height: 30),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _showInfo = !_showInfo;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('More Info'),
+              ),
+            ),
+            if (_showInfo)
+              Text(
+                'error type: ${widget.error.runtimeType}\n${widget.error.toString()}',
+                textAlign: TextAlign.center,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
