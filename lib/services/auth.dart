d45372827bb2b8021bef453935da8d39b9aa8640
Elfor <lshono8@gmail.com>
2021-05-22 16:09:46 +0300

    🎉 Added Google and Apple web signin

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
@@ -19,10 +20,34 @@ class AuthServices {
     });
   }
 
-  // The following method was imported from Flutterfire documentaion
+  /* ------------------ for Google Sign in ------------------ */
+
+  // The following methods was imported from Flutterfire documentaion
   // https://firebase.flutter.dev/docs/auth/social#google [accessed at 9th May 21]
 
   Future<UserCredential> signInWithGoogle() async {
+    if (kIsWeb) {
+      return await signInWithGoogleWeb();
+    } else {
+      return await signInWithGoogleNative();
+    }
+  }
+
+  Future<UserCredential> signInWithGoogleWeb() async {
+    // Create a new provider
+    GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+    googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+
+    // Or use signInWithRedirect
+    // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
+  }
+
+  Future<UserCredential> signInWithGoogleNative() async {
     // Trigger the authentication flow
     final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
 
@@ -47,21 +72,23 @@ class AuthServices {
   /* ------------------ for Apple Sign in ------------------ */
   // The following three methods were imported from Flutterfire documentaion
   // https://firebase.flutter.dev/docs/auth/social#apple [accessed at 9th May 21]
-
-  String generateNonce([int length = 32]) {
-    final charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
-    final random = Random.secure();
-    return List.generate(length, (_) => charset[random.nextInt(charset.length)]).join();
-  }
-
-  /// Returns the sha256 hash of [input] in hex notation.
-  String sha256ofString(String input) {
-    final bytes = utf8.encode(input);
-    final digest = sha256.convert(bytes);
-    return digest.toString();
-  }
-
   Future<UserCredential> signInWithApple() async {
+    if (kIsWeb) {
+      return await signInWithAppleWeb();
+    } else {
+      return await signInWithAppleIOS();
+    }
+  }
+
+  Future<UserCredential> signInWithAppleWeb() async {
+    // Create and configure an OAuthProvider for Sign In with Apple.
+    final provider = OAuthProvider("apple.com")..addScope('email')..addScope('name');
+
+    // Sign in the user with Firebase.
+    return await FirebaseAuth.instance.signInWithPopup(provider);
+  }
+
+  Future<UserCredential> signInWithAppleIOS() async {
     // To prevent replay attacks with the credential returned from Apple, we
     // include a nonce in the credential request. When signing in in with
     // Firebase, the nonce in the id token returned by Apple, is expected to
@@ -87,6 +114,19 @@ class AuthServices {
     // Sign in the user with Firebase. If the nonce we generated earlier does
     // not match the nonce in `appleCredential.identityToken`, sign in will fail.
     return await auth.signInWithCredential(oauthCredential);
+  }
+
+  String generateNonce([int length = 32]) {
+    final charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)]).join();
+  }
+
+  /// Returns the sha256 hash of [input] in hex notation.
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
   }
 
   /* ------------------ End of Apple Sign in methods ------------------ */
