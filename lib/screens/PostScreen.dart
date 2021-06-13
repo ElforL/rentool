efa9e0dcfe1e2f609982ebbc1eb2327872e66dc8
Elfor <lshono8@gmail.com>
2021-06-14 02:23:24 +0300

    ✨ Created post screen

---
+++ lib/screens/PostScreen.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:rentool_sdk/rentool_sdk.dart';
+
+class PostScreen extends StatefulWidget {
+  PostScreen({Key key, @required this.tool}) : super(key: key);
+
+  final Tool tool;
+
+  @override
+  _PostScreenState createState() => _PostScreenState();
+}
+
+class _PostScreenState extends State<PostScreen> {
+  PageController _mediaController;
+
+  @override
+  void initState() {
+    _mediaController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _mediaController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          // media
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            constraints: BoxConstraints(maxHeight: 200),
+            child: Stack(
+              children: [
+                PageView(
+                  controller: _mediaController,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    for (var url in widget.tool.media) Image.network(url),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _mediaController.previousPage(
+                          duration: Duration(milliseconds: 200),
+                          curve: Curves.ease,
+                        );
+                      },
+                      icon: Icon(Icons.arrow_left),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        _mediaController.nextPage(
+                          duration: Duration(milliseconds: 200),
+                          curve: Curves.ease,
+                        );
+                      },
+                      icon: Icon(Icons.arrow_right),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Divider(thickness: 1),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SelectableText(
+                  widget.tool.name,
+                  style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Text(
+                      'Price: ',
+                      style: Theme.of(context).textTheme.bodyText1.apply(color: Colors.grey.shade600),
+                    ),
+                    Text(
+                      'SAR ' + widget.tool.rentPrice.toString() + '/day',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Text(
+                      'Status: ',
+                      style: Theme.of(context).textTheme.bodyText1.apply(color: Colors.grey.shade600),
+                    ),
+                    Text(
+                      (widget.tool.isAvailable ? '' : 'Not ') + 'Available',
+                      style: TextStyle(color: widget.tool.isAvailable ? Colors.green : Colors.red),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Text(
+                      'Location: ',
+                      style: Theme.of(context).textTheme.bodyText1.apply(color: Colors.grey.shade600),
+                    ),
+                    Text(widget.tool.location),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Text(
+                      'Owner: ',
+                      style: Theme.of(context).textTheme.bodyText1.apply(color: Colors.grey.shade600),
+                    ),
+                    Text(widget.tool.ownerUID),
+                    // TODO owner rating
+                  ],
+                ),
+                SizedBox(height: 5),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.shopping_cart),
+                  label: Text('Request'),
+                  onPressed: !widget.tool.isAvailable ? null : () {},
+                ),
+              ],
+            ),
+          ), // end of main details
+          Divider(thickness: 1),
+          // Description
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Description',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                SizedBox(height: 15),
+                SelectableText(widget.tool.description),
+              ],
+            ),
+          ), // end of Description
+          Divider(thickness: 1),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/userScreen.dart
+++ lib/screens/userScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:rentool/screens/NewPostScreen.dart';
+import 'package:rentool/screens/PostScreen.dart';
 import 'package:rentool/services/auth.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rentool_sdk/rentool_sdk.dart';
 
 class UserScreen extends StatelessWidget {
   @override
@@ -36,6 +38,33 @@ class UserScreen extends StatelessWidget {
               onPressed: () {
                 //
                 Navigator.push(context, MaterialPageRoute(builder: (_) => NewPostScreen()));
+              },
+            ),
+          ),
+          ListTile(
+            trailing: OutlinedButton(
+              child: Text('view post'),
+              onPressed: () {
+                var tool = Tool(
+                  '42069',
+                  'xx420xx',
+                  'Philips Blender 700 W, 1.5lt glass jar, 5 speeds with crush technology, HR2222/01',
+                  '''5 pre-sets
+Removable lid & knife
+Crushed ice 2x faster
+700W, 1.5 liter glass jar''',
+                  23,
+                  120,
+                  [
+                    'https://images-na.ssl-images-amazon.com/images/I/714G2dHmcaL._AC_SL1500_.jpg',
+                    'https://images-na.ssl-images-amazon.com/images/I/71%2BRcdS6udL._AC_SL1500_.jpg',
+                    'https://images-na.ssl-images-amazon.com/images/I/81J%2Btj9dlSL._AC_SL1500_.jpg',
+                    'https://images-na.ssl-images-amazon.com/images/I/71Lyx3BixRL._AC_SL1301_.jpg',
+                  ],
+                  'Riyadh',
+                  true,
+                );
+                Navigator.push(context, MaterialPageRoute(builder: (_) => PostScreen(tool: tool)));
               },
             ),
           ),
