22495a2d42b33f1954d8ce9b6bca9a19f9a368e4
Elfor <lshono8@gmail.com>
2021-06-16 21:27:27 +0300

    ✨ added search button on userScreen

--- lib/screens/userScreen.dart
+++ lib/screens/userScreen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:rentool/screens/NewPostScreen.dart';
-import 'package:rentool/screens/PostScreen.dart';
-import 'package:rentool/services/auth.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:rentool_sdk/rentool_sdk.dart';
+import 'package:rentool/screens/NewPostScreen.dart';
+import 'package:rentool/screens/SearchScreen.dart';
+import 'package:rentool/services/auth.dart';
 
 class UserScreen extends StatelessWidget {
   @override
@@ -34,37 +33,19 @@ class UserScreen extends StatelessWidget {
           Divider(),
           ListTile(
             trailing: OutlinedButton(
-              child: Text('create post'),
+              child: Text('SEARCH'),
               onPressed: () {
                 //
-                Navigator.push(context, MaterialPageRoute(builder: (_) => NewPostScreen()));
+                Navigator.push(context, MaterialPageRoute(builder: (_) => SearchScreen()));
               },
             ),
           ),
           ListTile(
             trailing: OutlinedButton(
-              child: Text('view post'),
+              child: Text('CREATE POST'),
               onPressed: () {
-                var tool = Tool(
-                  '42069',
-                  'xx420xx',
-                  'Philips Blender 700 W, 1.5lt glass jar, 5 speeds with crush technology, HR2222/01',
-                  '''5 pre-sets
-Removable lid & knife
-Crushed ice 2x faster
-700W, 1.5 liter glass jar''',
-                  23,
-                  120,
-                  [
-                    'https://images-na.ssl-images-amazon.com/images/I/714G2dHmcaL._AC_SL1500_.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/71%2BRcdS6udL._AC_SL1500_.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/81J%2Btj9dlSL._AC_SL1500_.jpg',
-                    'https://images-na.ssl-images-amazon.com/images/I/71Lyx3BixRL._AC_SL1301_.jpg',
-                  ],
-                  'Riyadh',
-                  true,
-                );
-                Navigator.push(context, MaterialPageRoute(builder: (_) => PostScreen(tool: tool)));
+                //
+                Navigator.push(context, MaterialPageRoute(builder: (_) => NewPostScreen()));
               },
             ),
           ),
