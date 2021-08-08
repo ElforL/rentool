4e2fba55d427d9ad237995e53c16312fd0e503fd
Elfor <elfordev@gmail.com>
2021-08-08 17:24:55 +0300

    ☁ Disable FCM functions when running in Github Actions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,11 @@ import * as admin from 'firebase-admin';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
+const isOnGithubActions = process.env.GITHUB_ACTIONS == 'true';
+
 admin.initializeApp();
 
 export * from './other_firestore_triggers';
 export * from './meetings';
-export * from './fcm';
+if (!isOnGithubActions)
+    exports.fcm = require('./fcm');
