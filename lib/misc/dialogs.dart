01dc4902da76f890eef6951b8ff920c1b84bef4d
Elfor <elfordev@gmail.com>
2021-08-31 19:08:16 +0300

    ✨ Add `dialogs.dart` for pre-set `showDialog` functions

---
+++ lib/misc/dialogs.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:rentool/widgets/icon_alert_dialog.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+Future<dynamic> showDisagreementCasesHelpDialog(BuildContext context, bool isUserTheOwner) {
+  return showDialog(
+    context: context,
+    builder: (context) => IconAlertDialog(
+      icon: Icons.gavel_rounded,
+      titleText: AppLocalizations.of(context)!.disagreement_cases,
+      bodyText: isUserTheOwner
+          ? AppLocalizations.of(context)!.cases_help_dialog_owner_body
+          : AppLocalizations.of(context)!.cases_help_dialog_renter_body,
+      actions: [
+        TextButton(
+          child: Text(AppLocalizations.of(context)!.ok.toUpperCase()),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ],
+    ),
+  );
+}
