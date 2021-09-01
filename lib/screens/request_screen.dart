045a6b13a35b5721c97194a198ff91a9fd54e2af
Elfor <elfordev@gmail.com>
2021-09-01 11:51:07 +0300

    ✨ Create `RequestScreen`

---
+++ lib/screens/request_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:rentool/services/firestore.dart';
+import 'package:rentool_sdk/rentool_sdk.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class RequestScreen extends StatelessWidget {
+  const RequestScreen({
+    Key? key,
+    required this.request,
+    this.showAcceptButton = false,
+  }) : super(key: key);
+
+  final ToolRequest request;
+  final bool showAcceptButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              primary: false,
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.number_of_days,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(height: 15),
+                Text(request.numOfDays.toString()),
+                const SizedBox(height: 25),
+                Text(
+                  AppLocalizations.of(context)!.description,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(height: 15),
+                if (request.description.isNotEmpty)
+                  Text(request.description)
+                else
+                  Text(
+                    AppLocalizations.of(context)!.no_description,
+                    style: TextStyle(color: Colors.red.shade300),
+                  ),
+                const SizedBox(height: 25),
+                Text(
+                  AppLocalizations.of(context)!.renter,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(height: 15),
+                Text(request.renterUID),
+                const SizedBox(height: 25),
+                Text(
+                  AppLocalizations.of(context)!.price,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  AppLocalizations.of(context)!.rentPrice,
+                  style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '${request.numOfDays} ${AppLocalizations.of(context)!.days} × ${AppLocalizations.of(context)!.priceDisplay(
+                    AppLocalizations.of(context)!.sar,
+                    request.rentPrice,
+                  )} = ${AppLocalizations.of(context)!.priceDisplay(
+                    AppLocalizations.of(context)!.sar,
+                    request.rentPrice * request.numOfDays,
+                  )}',
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  AppLocalizations.of(context)!.insurancePrice,
+                  style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  AppLocalizations.of(context)!.priceDisplay(
+                    AppLocalizations.of(context)!.sar,
+                    request.insuranceAmount,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  AppLocalizations.of(context)!.total,
+                  style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  AppLocalizations.of(context)!.priceDisplay(
+                    AppLocalizations.of(context)!.sar,
+                    request.rentPrice * request.numOfDays + request.insuranceAmount,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (showAcceptButton) ...[
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    child: Text(AppLocalizations.of(context)!.reject.toUpperCase()),
+                    onPressed: () {
+                      FirestoreServices.deleteRequest(request.toolID, request.id);
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    child: Text(AppLocalizations.of(context)!.accept.toUpperCase()),
+                    onPressed: () {
+                      FirestoreServices.acceptRequest(request.toolID, request.id);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ]
+        ],
+      ),
+    );
+  }
+}
