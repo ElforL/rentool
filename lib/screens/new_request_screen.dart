9bd8822c593565cb69fb3fde7a1ba5352bd606ee
Elfor <elfordev@gmail.com>
2021-09-01 11:10:10 +0300

    🖌 Applied design from Figma to `NewRequestScreen`

--- lib/screens/new_request_screen.dart
+++ lib/screens/new_request_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/services.dart';
 import 'package:rentool/services/auth.dart';
 import 'package:rentool/services/firestore.dart';
@@ -19,7 +20,7 @@ class _NewRequestScreenState extends State<NewRequestScreen> {
   String? _descriptionErrorText;
   String? _daysErrorText;
 
-  int get daysNum => int.parse(_daysController.text);
+  int get daysNum => _daysController.text.isNotEmpty ? int.parse(_daysController.text) : 0;
 
   @override
   void initState() {
@@ -41,112 +42,132 @@ class _NewRequestScreenState extends State<NewRequestScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Request ${tool.name}'),
+        title: Text('${AppLocalizations.of(context)!.request} ${tool.name}'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
           children: [
-            Text.rich(TextSpan(
-              text: 'Price: ',
-              children: [
-                TextSpan(text: 'SAR ${tool.rentPrice}', style: const TextStyle(color: Colors.blue)),
-              ],
-            )),
-            const SizedBox(height: 10),
-            Text.rich(TextSpan(
-              text: 'Insurance deposit: ',
-              children: [
-                TextSpan(text: 'SAR ${tool.insuranceAmount}', style: const TextStyle(color: Colors.blue)),
-              ],
-            )),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _descriptionController,
-              textInputAction: TextInputAction.newline,
-              decoration: InputDecoration(
-                labelText: 'Description',
-                errorText: _descriptionErrorText,
-              ),
-              onChanged: (_) {
-                if (_descriptionErrorText != null) {
-                  _descriptionErrorText = null;
-                  setState(() {});
-                }
-              },
-            ),
-            TextField(
-              maxLength: 3,
-              controller: _daysController,
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: InputDecoration(
-                labelText: 'Number of days',
-                counterText: '',
-                errorText: _daysErrorText,
-              ),
-              onChanged: (_) {
-                if (_daysErrorText != null) _daysErrorText = null;
-                setState(() {});
-              },
-            ),
-            const SizedBox(height: 25),
-            Text.rich(TextSpan(
-              text: 'Rent: ',
-              children: [
-                TextSpan(text: 'SAR ${_calcTotal()}', style: const TextStyle(color: Colors.blue)),
-              ],
-            )),
-            const SizedBox(height: 10),
-            Text.rich(TextSpan(
-              text: 'Total with insurance: ',
-              children: [
-                TextSpan(text: 'SAR ${_calcTotal(true)}', style: const TextStyle(color: Colors.blue)),
-              ],
-            )),
-            Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+            Expanded(
+              child: ListView(
+                primary: false,
                 children: [
-                  OutlinedButton(
-                    child: const Text('CANCEL'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  const SizedBox(width: 20),
-                  ElevatedButton(
-                    child: const Text('SEND'),
-                    onPressed: () async {
-                      if (_daysController.text.isEmpty) {
-                        setState(() {
-                          _daysErrorText = "You can't leave this empty";
-                        });
-                      } else if (daysNum == 0) {
-                        setState(() {
-                          _daysErrorText = "Days must be 1 or greater";
-                        });
-                      } else {
-                        var request = ToolRequest(
-                          'TEMP',
-                          AuthServices.currentUid!,
-                          tool.id,
-                          _descriptionController.text,
-                          daysNum,
-                          tool.rentPrice,
-                          tool.insuranceAmount,
-                          false,
-                          false,
-                        );
-                        await FirestoreServices.sendNewToolRequest(request, tool.id);
-                        Navigator.pop(context);
+                  TextField(
+                    controller: _descriptionController,
+                    textInputAction: TextInputAction.newline,
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: AppLocalizations.of(context)!.description,
+                      errorText: _descriptionErrorText,
+                    ),
+                    onChanged: (_) {
+                      if (_descriptionErrorText != null) {
+                        _descriptionErrorText = null;
+                        setState(() {});
                       }
                     },
                   ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    maxLength: 3,
+                    controller: _daysController,
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: AppLocalizations.of(context)!.number_of_days,
+                      counterText: '',
+                      errorText: _daysErrorText,
+                    ),
+                    onChanged: (_) {
+                      if (_daysErrorText != null) _daysErrorText = null;
+                      setState(() {});
+                    },
+                  ),
+                  const SizedBox(height: 45),
+                  Text(
+                    AppLocalizations.of(context)!.summary,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    AppLocalizations.of(context)!.rentPrice,
+                    style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '$daysNum ${AppLocalizations.of(context)!.days} × ${AppLocalizations.of(context)!.priceDisplay(
+                      AppLocalizations.of(context)!.sar,
+                      tool.rentPrice,
+                    )} = ${AppLocalizations.of(context)!.priceDisplay(
+                      AppLocalizations.of(context)!.sar,
+                      _calcTotal(),
+                    )}',
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    AppLocalizations.of(context)!.insurancePrice,
+                    style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    AppLocalizations.of(context)!.priceDisplay(
+                      AppLocalizations.of(context)!.sar,
+                      tool.insuranceAmount,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    AppLocalizations.of(context)!.total,
+                    style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    AppLocalizations.of(context)!.priceDisplay(
+                      AppLocalizations.of(context)!.sar,
+                      _calcTotal(true),
+                    ),
+                  ),
                 ],
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton(
+                  child: Text(AppLocalizations.of(context)!.cancel.toUpperCase()),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  child: Text(AppLocalizations.of(context)!.send.toUpperCase()),
+                  onPressed: () async {
+                    if (_daysController.text.isEmpty) {
+                      setState(() {
+                        _daysErrorText = AppLocalizations.of(context)!.you_cant_leave_this_empty;
+                      });
+                    } else if (daysNum < 1) {
+                      setState(() {
+                        _daysErrorText = AppLocalizations.of(context)!.days_must_be_more_than_0;
+                      });
+                    } else {
+                      var request = ToolRequest(
+                        'TEMP',
+                        AuthServices.currentUid!,
+                        tool.id,
+                        _descriptionController.text,
+                        daysNum,
+                        tool.rentPrice,
+                        tool.insuranceAmount,
+                        false,
+                        false,
+                      );
+                      await FirestoreServices.sendNewToolRequest(request, tool.id);
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
+              ],
             ),
           ],
         ),
