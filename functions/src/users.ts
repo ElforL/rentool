5649fd9a332d7a4773223bd8e5fb025c030412c7
Elfor <elfordev@gmail.com>
2021-09-14 04:24:39 +0300

    ✨ Add `reviewWrite()`

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -22,3 +22,67 @@ export const userDocChange = functions.firestore.document('Users/{userID}').onUp
   });
 });
 
+export const reviewWrite = functions.firestore.document('Users/{userID}/reviews/{reviewID}')
+  .onWrite(async (change, context) => {
+    const uid = context.params.userID;
+    const isDelete = change.after.exists;
+    const isNew = change.before.exists;
+    const beforeData = change.before.data();
+    const afterData = change.after.data();
+
+    const userDoc = admin.firestore().doc(`Users/${uid}`);
+
+
+    if (isDelete) {
+      return admin.firestore().runTransaction(async (transaction) => {
+        // Read the data
+        const userData = await transaction.get(userDoc);
+
+        // Process
+        const rating = userData.data()!.rating;
+        const numOfReviews = userData.data()!.numOfReviews;
+
+        const reviewValue = beforeData!.value;
+
+        const totalWithoutVal = (rating * numOfReviews) - reviewValue;
+        const avgAfter = totalWithoutVal / (numOfReviews - 1);
+
+        // Update
+        return transaction.update(userDoc, {
+          'rating': avgAfter,
+          'numOfReviews': numOfReviews - 1,
+        });
+      })
+    } else {
+      return admin.firestore().runTransaction(async (transaction) => {
+        // Read the data
+        const userData = await transaction.get(userDoc);
+
+        // Process
+        const rating = userData.data()!.rating;
+        const numOfReviews = userData.data()!.numOfReviews;
+        const reviewValue = afterData!.value;
+
+        const oldTotal = rating * numOfReviews;
+
+        var newAvg;
+        var newNumOfReviews;
+        if (isNew) {
+          // New review
+          newAvg = (oldTotal + reviewValue) / (numOfReviews + 1);
+          newNumOfReviews = numOfReviews + 1;
+        } else {
+          // change review value
+          const oldReviewValue = beforeData!.value;
+          newAvg = ((oldTotal - oldReviewValue) + reviewValue) / numOfReviews;
+          newNumOfReviews = numOfReviews;
+        }
+
+        // Update
+        return transaction.update(userDoc, {
+          'rating': newAvg,
+          'numOfReviews': newNumOfReviews,
+        });
+      })
+    }
+  });
