338039089bc1969779e5792ff362d15e29b63e18
Elfor <elfordev@gmail.com>
2021-10-10 18:42:44 +0300

    🖌 Improve Tool tile responsiveness and design

--- lib/widgets/tool_tile.dart
+++ lib/widgets/tool_tile.dart
@@ -17,6 +17,29 @@ class ToolTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    Widget? image;
+    if (tool.media.isNotEmpty) {
+      image = ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.network(
+          tool.media.first,
+          errorBuilder: (context, error, stackTrace) {
+            return FutureBuilder(
+              future: VideoThumbnail.thumbnailData(video: tool.media.first),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return Image.memory(snapshot.data as Uint8List);
+              },
+            );
+          },
+        ),
+      );
+    }
+
     return InkWell(
       onTap: onTap,
       child: Container(
@@ -24,53 +47,53 @@ class ToolTile extends StatelessWidget {
         child: Row(
           children: [
             Expanded(
-              child: tool.media.isNotEmpty
-                  ? Image.network(
-                      tool.media.first,
-                      errorBuilder: (context, error, stackTrace) {
-                        return FutureBuilder(
-                          future: VideoThumbnail.thumbnailData(video: tool.media.first),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState != ConnectionState.done) {
-                              return const Center(child: CircularProgressIndicator());
-                            }
-                            return Image.memory(snapshot.data as Uint8List);
-                          },
-                        );
-                      },
-                    )
-                  : const Icon(Icons.image_not_supported),
+              child: Container(
+                constraints: const BoxConstraints(maxHeight: 150),
+                child: image ?? const Icon(Icons.image_not_supported),
+              ),
             ),
-            const SizedBox(width: 10),
+            const SizedBox(width: 20),
             Expanded(
-              flex: 4,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Tool name
-                  Text(
-                    tool.name,
-                    style: Theme.of(context).textTheme.headline6,
+              flex: size.width >= 960
+                  ? 9
+                  : size.width >= 500
+                      ? 4
+                      : 2,
+              child: Align(
+                alignment: AlignmentDirectional.topStart,
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 500),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Tool name
+                      Text(
+                        tool.name + 'dsadad ' * 100,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      // price
+                      Text(
+                        AppLocalizations.of(context)!.priceADay(
+                          AppLocalizations.of(context)!.sar,
+                          tool.rentPrice.toString(),
+                        ),
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                      // available
+                      Text(
+                        tool.isAvailable
+                            ? AppLocalizations.of(context)!.available
+                            : AppLocalizations.of(context)!.notAvailable,
+                        style: TextStyle(
+                          color: tool.isAvailable ? Colors.green : Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                  // price
-                  Text(
-                    AppLocalizations.of(context)!.priceADay(
-                      AppLocalizations.of(context)!.sar,
-                      tool.rentPrice.toString(),
-                    ),
-                    style: Theme.of(context).textTheme.caption,
-                  ),
-                  // available
-                  Text(
-                    tool.isAvailable
-                        ? AppLocalizations.of(context)!.available
-                        : AppLocalizations.of(context)!.notAvailable,
-                    style: TextStyle(
-                      color: tool.isAvailable ? Colors.green : Colors.red,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
