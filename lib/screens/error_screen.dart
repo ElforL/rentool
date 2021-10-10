12809e04b4f2321ccc474ca82b4f021f86b50757
Elfor <elfordev@gmail.com>
2021-10-10 22:55:40 +0300

    ✨ Add `ErrorScreen`

---
+++ lib/screens/error_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({
+    Key? key,
+    required this.error,
+  }) : super(key: key);
+
+  final Object? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.error),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.error, size: 100),
+              const SizedBox(height: 20),
+              Text(
+                AppLocalizations.of(context)!.unexpected_error_occured,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Theme(
+                data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+                child: ExpansionTile(
+                  tilePadding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 4),
+                  childrenPadding: const EdgeInsets.symmetric(horizontal: 15),
+                  title: Text(AppLocalizations.of(context)!.errorInfo),
+                  children: [SelectableText(error.toString())],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
