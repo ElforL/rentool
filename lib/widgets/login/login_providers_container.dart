da1671961a4b173ad739e565369b36019fa16e6f
Elfor <elfordev@gmail.com>
2021-10-10 21:45:36 +0300

    📝 remove localize todos from sign-in buttons

--- lib/widgets/login/login_providers_container.dart
+++ lib/widgets/login/login_providers_container.dart
@@ -36,9 +36,9 @@ class AuthProvidersContainer extends StatelessWidget {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // These buttons aren't localized due to the providers' guidlines
             AuthProviderButton(
               icon: Image.asset('assets/images/google_icon.png'),
-              // TODO localize?
               label: const Text(
                 'SIGN IN WITH GOOGLE',
                 style: TextStyle(fontFamily: 'Roboto'),
@@ -52,7 +52,6 @@ class AuthProvidersContainer extends StatelessWidget {
             ),
             AuthProviderButton(
               icon: Image.asset('assets/images/Facebook_icon.png'),
-              // TODO localize?
               label: const Text(
                 'Login with Facebook',
                 style: TextStyle(fontFamily: 'Roboto'),
@@ -66,7 +65,6 @@ class AuthProvidersContainer extends StatelessWidget {
             ),
             AuthProviderButton(
               icon: Image.asset('assets/images/microsoft_icon.png'),
-              // TODO localize?
               label: const Text(
                 'Sign in with Microsoft',
                 style: TextStyle(fontFamily: 'Roboto'),
