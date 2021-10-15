86cc900e02c2cf166314638f1b57e5a45e9169d6
Elfor <elfordev@gmail.com>
2021-10-15 17:52:21 +0300

    ✨Add `constants.dart` containing checkoutPublicKey

---
+++ lib/misc/constants.dart
@@ -0,0 +1,4 @@
+/// The public key for Checkout account
+///
+/// Can be accessed and regenerated in [the hub](https://sandbox.checkout.com/settings/channels)
+const checkoutPublicKey = 'pk_test_7f57b647-99bb-4d9f-b670-60de7118c75c';
