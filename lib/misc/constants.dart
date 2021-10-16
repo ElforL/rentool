483c0afee9664e2630481d3184cfb19e211bfb62
Elfor <elfordev@gmail.com>
2021-10-16 19:20:05 +0300

    📝 change Checkout public key use the rentool.site channel

--- lib/misc/constants.dart
+++ lib/misc/constants.dart
@@ -1,4 +1,4 @@
 /// The public key for Checkout account
 ///
 /// Can be accessed and regenerated in [the hub](https://sandbox.checkout.com/settings/channels)
-const checkoutPublicKey = 'pk_test_7f57b647-99bb-4d9f-b670-60de7118c75c';
+const checkoutPublicKey = 'pk_test_30d3f3b2-9024-4f31-ba34-0dc393d9080d';
