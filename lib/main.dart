f8cc49a5b6dd88ae49adb32543f273b95b18af13
Elfor <elfordev@gmail.com>
2021-11-07 20:44:18 +0300

    🐛 Disable dynamic links for web since it's not supported

--- lib/main.dart
+++ lib/main.dart
@@ -159,7 +159,7 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     _locale = widget.locale;
     super.initState();
-    initDynamicLinks();
+    if (!kIsWeb) initDynamicLinks();
   }
 
   @override
