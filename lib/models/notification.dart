866022940741e414b5122c1fc0a46d4a8f725aba
Elfor <elfordev@gmail.com>
2021-11-08 19:34:52 +0300

    ✨ Add rentoolNotification.getOnPressHandler()

--- lib/models/notification.dart
+++ lib/models/notification.dart
@@ -1,12 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rentool/screens/post_screen.dart';
+import 'package:rentool/screens/user_screen.dart';
 import 'package:rentool/services/auth.dart';
 import 'package:rentool/services/firestore.dart';
 
 class RentoolNotification {
   final String id;
   final String code;
+
+  /// - `REQ_REC`: toolID, requestID, toolName, renterName
+  /// - `REQ_ACC`: toolID, requestID, toolName,
+  /// - `REQ_DEL`: toolID, requestID, toolName,
+  /// - `REN_START`: toolID, toolName, renterName, ownerName, renterUID
+  /// - `REN_END`: toolID, toolName, otherUserName, otherUserId, notificationBodyArgs
+  /// - `DC_DAM`: toolID, toolName
+  /// - `DC_NDAM`: toolID, toolName
   final Map<String, dynamic> data;
   bool isRead;
   final DateTime time;
@@ -65,6 +75,27 @@ class RentoolNotification {
         return AppLocalizations.of(context)!.body_DC_DAM(data['toolName']);
       case 'DC_NDAM':
         return AppLocalizations.of(context)!.body_DC_NDAM(data['toolName']);
+      default:
+        throw ArgumentError("Code doesn't match any notification code: $code");
+    }
+  }
+
+  Function() getOnPressHandler(BuildContext context) {
+    switch (code) {
+      case 'REQ_REC':
+        return () => Navigator.of(context).pushNamed(PostScreen.routeName + '/${data['toolID']}');
+      case 'REQ_ACC':
+        return () => Navigator.of(context).pushNamed(PostScreen.routeName + '/${data['toolID']}');
+      case 'REQ_DEL':
+        return () => Navigator.of(context).pushNamed(PostScreen.routeName + '/${data['toolID']}');
+      case 'REN_START':
+        return () => Navigator.of(context).pushNamed(PostScreen.routeName + '/${data['toolID']}');
+      case 'REN_END':
+        return () => Navigator.of(context).pushNamed(UserScreen.routeName + '/${data['otherUserId']}');
+      case 'DC_DAM':
+        return () => Navigator.of(context).pushNamed(PostScreen.routeName + '/${data['toolID']}');
+      case 'DC_NDAM':
+        return () => Navigator.of(context).pushNamed(PostScreen.routeName + '/${data['toolID']}');
       default:
         throw ArgumentError("Code doesn't match any notification code: $code");
     }
