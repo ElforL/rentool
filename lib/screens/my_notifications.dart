ca164cf084997f146ca00146eab5a2f576940d7d
Elfor <elfordev@gmail.com>
2021-11-08 19:35:22 +0300

    ✨ Pressing a notification navigates to a screen

--- lib/screens/my_notifications.dart
+++ lib/screens/my_notifications.dart
@@ -122,6 +122,7 @@ class _MyNotificationsScreenState extends State<MyNotificationsScreen> {
                     style: notification.isRead ? null : const TextStyle(fontWeight: FontWeight.bold),
                   ),
                   onTap: () {
+                    notification.getOnPressHandler(context)();
                     setState(() {
                       notification.setIsRead();
                     });
